5a20ae8f137813aaaeae5014aba988a68410140d
bharath <dasari@gmail.com>
2024-08-29 10:21:00 +0000

    sample text

---
+++ file.py
@@ -0,0 +1,7 @@
+print("welcome to travels")
+print(" Enter your name:  ")
+print(" Enter your age: ")
+print(" Enter your no: ")
+print(" Enter your starting point:")
+print(" Enter your dropping point:")
+
